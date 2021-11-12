3c0a82d3d89809b04f5139248c5f7a4bb43de3ec
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2021-11-12 13:48:58 +0530

    --Repository creation with the Dao

---
+++ app/src/main/java/com/example/to_do/data/repository/TodoRepository.kt
@@ -0,0 +1,37 @@
+package com.example.to_do.data.repository
+
+import com.example.to_do.data.TodoDao
+import com.example.to_do.data.models.TodoTask
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class TodoRepository @Inject constructor(private val todoDao: TodoDao) {
+
+    val getAllTask: Flow<List<TodoTask>> = todoDao.getAllTask()
+    val sortByLowPriority: Flow<List<TodoTask>> = todoDao.sortByLowPriority()
+    val sortByHighPriority: Flow<List<TodoTask>> = todoDao.sortByHighPriority()
+
+    fun getSelectedTask(taskId: Int): Flow<TodoTask> {
+        return todoDao.getSelectedTask(taskId = taskId)
+    }
+
+    suspend fun addTask(todoTask: TodoTask) {
+        return todoDao.addTask(todoTask = todoTask)
+    }
+
+    suspend fun updateTask(todoTask: TodoTask) {
+        return todoDao.updateTask(todoTask = todoTask)
+    }
+
+    suspend fun deleteTask(todoTask: TodoTask) {
+        return todoDao.deleteTask(todoTask = todoTask)
+    }
+
+    suspend fun deleteAllTask() {
+        return todoDao.deleteAllTask()
+    }
+
+    fun searchDatabase(searchQuery: String): Flow<List<TodoTask>> {
+        return todoDao.searchDatabase(searchQuery = searchQuery)
+    }
+}
